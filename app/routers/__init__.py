ea9c685e9c78c25e7e1fba9d677919c99f7a62db
omegarevolt <vysochin.sergei2009@gmail.com>
2025-12-27 18:39:22 +0300

    [Refactor] Create unified router in app/routers/__init__.py

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,1 +1,28 @@
-"""Routers package - all bot handlers"""
+"""Unified router - combines all bot handlers"""
+
+from aiogram import Router
+from app.middleware.admin import AdminMiddleware
+
+from .admin import router as admin_router
+from .user import setup_handlers as setup_user_handlers
+
+__all__ = ['router']
+
+
+def get_router() -> Router:
+    """Get unified router with all handlers"""
+    main_router = Router()
+    
+    # Add admin middleware to admin router
+    admin_router.message.middleware(AdminMiddleware())
+    admin_router.callback_query.middleware(AdminMiddleware())
+    
+    # Include all routers
+    main_router.include_router(admin_router)
+    main_router.include_router(setup_user_handlers())
+    
+    return main_router
+
+
+# Export unified router
+router = get_router()
